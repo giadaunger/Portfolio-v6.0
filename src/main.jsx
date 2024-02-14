14044171070abbee56dcbc9cb24f4bd682d035a3
Giada Unger <giada.unger02@gmail.com>
2024-02-14 13:41:55 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
