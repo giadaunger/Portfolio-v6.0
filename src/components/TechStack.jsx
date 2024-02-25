8bcc803682fddde34e2a33b1cb861a0c85d22089
Giada Unger <giada.unger02@gmail.com>
2024-02-25 17:39:37 +0100

    Changed the height for the tech stack divs

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function TechStack() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-9 text-[#ADA7AD] font-semibold">
-      <div className="border border-[#ADA7AD] p-2 h-auto relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
+      <div className="border border-[#ADA7AD] p-2 h-20 relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
         <p className="text-center">Javascript</p>
         <div className="dropdown-content hidden absolute left-0 top-full mt-1 bg-white border border-[#ff5277] text-[#ff5277] p-2 group-hover:block w-full text-center z-20">
           <p>React</p>
@@ -12,7 +12,7 @@ function TechStack() {
           <p>TypeScript</p>
         </div>
       </div>
-      <div className="border border-[#ADA7AD] p-2 h-auto relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
+      <div className="border border-[#ADA7AD] p-2 h-20 relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
         <p className="text-center">Python</p>
         <div className="dropdown-content hidden absolute left-0 top-full mt-1 bg-white border border-[#ff5277] text-[#ff5277] p-2 group-hover:block w-full text-center">
           <p>FastAPI</p>
@@ -23,13 +23,13 @@ function TechStack() {
       <div className="border border-[#ADA7AD] p-2 h-20 flex justify-center items-center">
         <p className="text-center">C++</p>
       </div>
-      <div className="border border-[#ADA7AD] p-2 h-auto relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
+      <div className="border border-[#ADA7AD] p-2 h-20 relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
         <p className="text-center">SQL</p>
         <div className="dropdown-content hidden absolute left-0 top-full mt-1 bg-white border border-[#ff5277] text-[#ff5277] p-2 group-hover:block w-full text-center">
           <p>PostgreSQL</p>
         </div>
       </div>
-      <div className="border border-[#ADA7AD] p-2 h-auto relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
+      <div className="border border-[#ADA7AD] p-2 h-20 relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
         <p className="text-center">DevOps</p>
         <div className="dropdown-content hidden absolute left-0 top-full mt-1 bg-white border border-[#ff5277] text-[#ff5277] p-2 group-hover:block w-full text-center">
           <p>Linux, Ubuntu</p>
@@ -42,7 +42,7 @@ function TechStack() {
       <div className="border border-[#ADA7AD] p-2 h-20 flex justify-center items-center">
         <p className="text-center">UX/UI</p>
       </div>
-      <div className="border border-[#ADA7AD] p-2 h-auto relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
+      <div className="border border-[#ADA7AD] p-2 h-20 relative group flex justify-center items-center hover:text-[#ff5277] hover:border-[#ff5277] transition duration-300 hover:scale-110">
         <p className="text-center">Styling Frameworks</p>
         <div className="dropdown-content hidden absolute left-0 top-full mt-1 bg-white border border-[#ff5277] text-[#ff5277] p-2 group-hover:block w-full text-center">
           <p>Chakra UI</p>
